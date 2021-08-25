5784462d64d3f1854514780a4b58b45a6e916ba4
brunoBMonteiro <brunobilheri@hotmail.com>
2021-08-25 11:56:58 -0300

    exercise 5 concluded

--- exercicioJs/exercicio05.js
+++ exercicioJs/exercicio05.js
@@ -2,4 +2,14 @@
 retorna a mesma string com a primeira letra em maiúscula. Utilize corretamente a declaração de tipos nos 
 parâmetros e no resultado da função. 
 */
+function toMaiusculaPrimeira (name) {
 
+    name = name.toLowerCase().replace(/(?:^|\s)\S/g, 
+    
+    function(toMaiusculaPrimeira) {
+      return toMaiusculaPrimeira.toUpperCase();
+    });
+    console.log(name);
+    }
+    
+    toMaiusculaPrimeira("bruno")
